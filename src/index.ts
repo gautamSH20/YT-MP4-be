adc9bab7125026b9ec4490e924ae4f3fe69da196
gautamSH20 <gs841400@gmail.com>
2025-01-29 15:22:58 +0530

    new backend

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import ytdl from "@distube/ytdl-core";
+import ffmpeg from "fluent-ffmpeg";
+import ffmpegStatic from "ffmpeg-static";
+import { PassThrough } from "stream";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post(
+  "/api/v1/download",
+  async (req: Request, res: Response): Promise<any> => {
+    try {
+      const { url } = req.body;
+
+      if (ytdl.validateURL(url)) {
+        const audioStream = ytdl(url, { quality: "highestaudio" });
+
+        const ffmpegProcess = ffmpeg(audioStream)
+          .setFfmpegPath(ffmpegStatic as string)
+          .audioBitrate(128)
+          .format("mp3")
+          .on("error", (err) => {
+            console.error(`something is wrong ${err}`);
+            res.status(500).end();
+          });
+        const stream = new PassThrough();
+        const response = ffmpegProcess.pipe(stream);
+
+        res.header(
+          "Content-Disposition",
+          'attachment; filename="downloaded.mp3"'
+        );
+
+        stream.pipe(res);
+      } else {
+        return res.status(400).json({
+          message: "something is wrong",
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      return res.status(500).json({
+        message: "something is wrong",
+      });
+    }
+  }
+);
+
+app.listen(3001, () => {
+  console.log(`this is working`);
+});
